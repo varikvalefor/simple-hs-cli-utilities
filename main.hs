5f1a5c8bde7f1948c96c74661a5ebf01f3a9a410
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-08 18:48:08 -0500

    main.hs: cat is added.

--- main.hs
+++ main.hs
@@ -5,8 +5,12 @@ pwd :: IO ();
 pwd = getEnvironment >>= \env ->
   putStrLn $ snd $ filter ((== "PWD") . fst) env !! 0;
 
+cat :: IO ();
+cat = getArgs >>= mapM_ (readFile >=> putStr);
+
 decidePrg :: String -> IO ();
 decidePrg x | x == "pwd" = pwd
+            | x == "cat" = cat
 
 main :: IO ();
 main = getProgName >>= decidePrg;
