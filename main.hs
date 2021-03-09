ba14b82ed1be214017b266f77a9db6069ce2ccfe
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-08 19:00:21 -0500

    main.hs: A shitty version of wc is implemented.

--- main.hs
+++ main.hs
@@ -12,10 +12,14 @@ whoami :: IO ();
 whoami = getEnvironment >>= putStrLn . snd . (!!0) .
   filter ((== "USER") . fst);
 
+wc :: IO ();
+wc = getArgs >>= mapM_ (readFile >=> putStrLn . show . length . words);
+
 decidePrg :: String -> IO ();
 decidePrg x | x == "pwd" = pwd
             | x == "cat" = cat
             | x == "whoami" = whoami
+            | x == "wc" = wc
 
 main :: IO ();
 main = getProgName >>= decidePrg;
